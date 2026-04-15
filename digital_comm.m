6aeea675602cda8a3da754bef799524f482cc9c8
naderhany <naderhany7777@gmail.com>
2026-04-15 23:59:28 +0200

    creating The structure of the code

---
+++ digital_comm.m
@@ -0,0 +1,74 @@
+%===========Define parameters=========%
+
+
+
+
+
+
+
+
+
+%==========Creating random_data=======%
+
+
+
+
+
+
+%===========Unipolar Ensemble==========%
+%Transmitter%
+
+
+
+
+
+
+%Receiver(Mean, Autocorrelation, Ergodicity, BW)%
+
+
+
+
+%Results%
+
+
+
+
+%============polar NRZ Ensemble==========%
+%Transmitter%
+
+
+
+
+
+
+%Receiver(Mean, Autocorrelation, Ergodicity, BW)%
+
+
+
+
+%Results%
+
+
+
+%==============of polar RZ Ensemble========%
+%Transmitter%
+
+
+
+
+
+
+%Receiver(Mean, Autocorrelation, Ergodicity, BW)%
+
+
+
+
+%Results%
+
+
+
+
+
+
+
+
